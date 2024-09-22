75fd179bcacb09dabc2265b15aeb7f0c3e3bb41b
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-09-22 23:08:26 +0800

    init

---
+++ print/main.s
@@ -0,0 +1,18 @@
+hello:
+	.string "Hello, World!\n"
+
+.global _start
+.intel_syntax noprefix
+
+_start:
+	// sys_write
+	mov rax, 1
+	mov rdi, 1
+	lea rsi, [hello]
+	mov rdx, 14
+	syscall
+
+	// sys_exit
+	mov rax, 60
+	mov rdi, 0
+	syscall
