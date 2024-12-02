495d7fa5a3efce03602f51fb5d5775296ec12d25
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-12-03 03:21:32 +0800

    Update paint.asm
    Added comments

--- a86/paint.asm
+++ a86/paint.asm
@@ -4,21 +4,21 @@ call paint_start
 int 20h
 
 paint_start:
-	mov ah, 1h
-	int 21h
-	cmp al, 30h
+	mov ah, 1h					; Wait for key to be pressed
+	int 21h						; Call the interrupt to get the key
+	cmp al, 30h					; Check if the key is 0, if it is, exit
 	je paint_end
 	call change_color
 
 	call get_mouse_index
-	mov byte [ds:di + 1], al
+	mov byte [ds:di + 1], al	; Change the color of the pixel
 
 	jmp paint_start
 paint_end:
-	mov dl, 0Dh
+	mov dl, 0Dh					; Carriage return character (move to the beginning of the line)
 	mov ah, 02h
 	int 21h
-	mov dl, 0Ah
+	mov dl, 0Ah					; New line character (move to the next line)
 	mov ah, 02h
 	int 21h
 	ret
@@ -43,16 +43,16 @@ change_color:
 		ret
 
 get_mouse_index:
-	push ax
-	mov ax, 3h
-	int 33h
+	push ax						; Save the value of ax, since this was for the color to be changed
+	mov ah, 3h					; Get the mouse position
+	int 33h						; Call the interrupt to get the mouse position
 
-	shr cx, 2
-	shr dx, 2
+	shr cx, 2					; Divide by 4 to get the x coordinate
+	shr dx, 2					; Divide by 4 to get the y coordinate
 
-	imul dx, 80
-	add dx, cx
+	imul dx, 80					; Multiply by 80 to get the index
+	add dx, cx					; Add the x coordinate to the index
 
-	mov di, dx
-	pop ax
+	mov di, dx					; Move the index to di
+	pop ax						; Restore the value of ax
 	ret
