2e1b63496b5be617ffd9259f0b62e9d961251028
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-12-05 02:48:17 +0800

    Update add-two-sum.asm

--- a86/add-two-sum.asm
+++ a86/add-two-sum.asm
@@ -45,7 +45,7 @@ print_loop:
     jnz print_loop
 print_digits:
     pop dx                  ; Pop the digit from the stack
-    mov ah, 02h             ; DOS interrupt to print the character, register dl will be used here, that is why we are using the register dx
+    mov ah, 02h             ; DOS interrupt to print the character, register dl will be used here, that is why we are storing the register dx
     int 21h                 ; Call the interrupt
     loop print_digits       ; Where register cx is being used, we could just simply check if sp (stack) is empty, but why bother
     mov dl, 0Dh             ; Carriage return character (move to the beginning of the line)
